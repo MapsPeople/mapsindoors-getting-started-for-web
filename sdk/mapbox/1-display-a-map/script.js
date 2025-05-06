2f69382e2a471bd5c6933135d5e1b878be5a9b9b
Esben Nepper <en@mapspeople.com>
2025-05-06 13:50:26 +0200

    feat: Implement initial Mapbox setup with HTML, CSS, and JavaScript files

---
+++ sdk/mapbox/1-display-a-map/script.js
@@ -0,0 +1,16 @@
+const mapViewOptions = {
+    accessToken: 'pk.eyJ1IjoibWFwc3Blb3BsZSIsImEiOiJjbHV0cWo2MGUwMDA2MmtyeXV0cmZpcXQ4In0.iOwpJY4KqHVQoxA1UPEvgQ',
+    element: document.getElementById('map'),
+    center: { lat: 30.359285384, lng: -97.7412840716576 }, // MapsPeople - Austin Office
+    zoom: 17,
+    maxZoom: 22
+};
+mapsindoors.MapsIndoors.setMapsIndoorsApiKey('d876ff0e60bb430b8fabb145');
+const mapViewInstance = new mapsindoors.mapView.MapboxView(mapViewOptions);
+const mapsIndoorsInstance = new mapsindoors.MapsIndoors({ mapView: mapViewInstance });
+const mapboxInstance = mapViewInstance.getMap();
+
+// Floor Selector
+const floorSelectorElement = document.createElement('div');
+new mapsindoors.FloorSelector(floorSelectorElement, mapsIndoorsInstance);
+mapboxInstance.addControl({ onAdd: function () { return floorSelectorElement }, onRemove: function () { } });
