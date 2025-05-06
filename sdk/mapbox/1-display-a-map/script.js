adc8cc41b5ed0ee358d930f180707604ee19e140
Esben Nepper <en@mapspeople.com>
2025-05-06 15:14:04 +0200

    refactor: Clean up formatting and structure in script.js for better readability

--- sdk/mapbox/1-display-a-map/script.js
+++ sdk/mapbox/1-display-a-map/script.js
@@ -4,24 +4,23 @@ const mapViewOptions = {
     center: { lng: -97.74204591828197, lat: 30.36022358949809 }, // MapsPeople - Austin Office
     zoom: 17.1,
     maxZoom: 22,
-  };
-  
-  mapsindoors.MapsIndoors.setMapsIndoorsApiKey('02c329e6777d431a88480a09');
-  
-  const mapViewInstance = new mapsindoors.mapView.MapboxV3View(mapViewOptions);
-  const mapsIndoorsInstance = new mapsindoors.MapsIndoors({
-    mapView: mapViewInstance,
-  });
+};
 
-  const mapboxInstance = mapViewInstance.getMap();
-  
-  // Floor Selector
-  const floorSelectorElement = document.createElement('div');
-  new mapsindoors.FloorSelector(floorSelectorElement, mapsIndoorsInstance);
-  mapboxInstance.addControl({
+mapsindoors.MapsIndoors.setMapsIndoorsApiKey('02c329e6777d431a88480a09');
+
+const mapViewInstance = new mapsindoors.mapView.MapboxV3View(mapViewOptions);
+const mapsIndoorsInstance = new mapsindoors.MapsIndoors({
+    mapView: mapViewInstance,
+});
+
+const mapboxInstance = mapViewInstance.getMap();
+
+// Floor Selector
+const floorSelectorElement = document.createElement('div');
+new mapsindoors.FloorSelector(floorSelectorElement, mapsIndoorsInstance);
+mapboxInstance.addControl({
     onAdd: function () {
-      return floorSelectorElement;
+        return floorSelectorElement;
     },
-    onRemove: function () {},
-  });
-  
+    onRemove: function () { },
+});
