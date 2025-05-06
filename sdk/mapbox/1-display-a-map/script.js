395be7d14073329039d289fd7036eeb120c849f8
Esben Nepper <en@mapspeople.com>
2025-05-06 14:41:27 +0200

    fix: Correct mapViewOptions center coordinates and update MapsIndoors API key

--- sdk/mapbox/1-display-a-map/script.js
+++ sdk/mapbox/1-display-a-map/script.js
@@ -1,17 +1,27 @@
 const mapViewOptions = {
     accessToken: 'pk.eyJ1IjoiZW5lcHBlciIsImEiOiJja2c5MzlqaDIwNTgwMnhwdWg5M2gycXQyIn0.3yzc8Se6Majxr2O09HNGiA',
     element: document.getElementById('map'),
-    center: { lat: 30.359285384, lng: -97.7412840716576 }, // MapsPeople - Austin Office
-    zoom: 17,
-    maxZoom: 22
-};
-mapsindoors.MapsIndoors.setMapsIndoorsApiKey('d876ff0e60bb430b8fabb145');
+    center: { lng: -97.74204591828197, lat: 30.36022358949809 }, // MapsPeople - Austin Office
+    zoom: 17.1,
+    maxZoom: 22,
+  };
+  
+  mapsindoors.MapsIndoors.setMapsIndoorsApiKey('02c329e6777d431a88480a09');
+  
+  const mapViewInstance = new mapsindoors.mapView.MapboxV3View(mapViewOptions);
+  const mapsIndoorsInstance = new mapsindoors.MapsIndoors({
+    mapView: mapViewInstance,
+  });
 
-const mapViewInstance = new mapsindoors.mapView.MapboxV3View(mapViewOptions);
-const mapsIndoorsInstance = new mapsindoors.MapsIndoors({ mapView: mapViewInstance });
-const mapboxInstance = mapViewInstance.getMap();
-
-// Floor Selector
-const floorSelectorElement = document.createElement('div');
-new mapsindoors.FloorSelector(floorSelectorElement, mapsIndoorsInstance);
-mapboxInstance.addControl({ onAdd: function () { return floorSelectorElement }, onRemove: function () { } });
+  const mapboxInstance = mapViewInstance.getMap();
+  
+  // Floor Selector
+  const floorSelectorElement = document.createElement('div');
+  new mapsindoors.FloorSelector(floorSelectorElement, mapsIndoorsInstance);
+  mapboxInstance.addControl({
+    onAdd: function () {
+      return floorSelectorElement;
+    },
+    onRemove: function () {},
+  });
+  
