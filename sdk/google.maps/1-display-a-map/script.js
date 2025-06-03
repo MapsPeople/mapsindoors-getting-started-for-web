ee5de543e2cb8d533fd400734a2bdc00392a0334
Esben Nepper <en@mapspeople.com>
2025-06-03 13:54:17 +0200

    fix: Update HTML structure and improve script comments for clarity in index.html and script.js

--- sdk/google.maps/1-display-a-map/script.js
+++ sdk/google.maps/1-display-a-map/script.js
@@ -1,35 +1,25 @@
-// script.js
-
-// Define options for the Google Maps view
+// Define options for the MapsIndoors Google Maps view
 const mapViewOptions = {
     element: document.getElementById('map'),
-    // MapsPeople - Austin Office
-    center: { lat: 30.36026660239549, lng: -97.74223633857213 },
+    center: { lng: -97.74204591828197, lat: 30.36022358949809 },
     zoom: 17,
     maxZoom: 22,
 };
 
-//Set the MapsIndoors API key
-mapsindoors.MapsIndoors.setMapsIndoorsApiKey('02c329e6777d431a88480a09');
+// Set the MapsIndoors API key
+mapsindoors.MapsIndoors.setMapsIndoorsApiKey('YOUR_MAPSINDOORS_API_KEY');
 
-// Create a new instance of the Google Maps view
+// Create a new instance of the MapsIndoors Google Maps view
 const mapViewInstance = new mapsindoors.mapView.GoogleMapsView(mapViewOptions);
 
-// Create a new MapsIndoors instance, passing the map view
+// Create a new MapsIndoors instance, linking it to the map view
 const mapsIndoorsInstance = new mapsindoors.MapsIndoors({
     mapView: mapViewInstance,
-    // Set the venue ID to load the map for a specific venue
-    venue: 'dfea941bb3694e728df92d3d',
+    venue: 'YOUR_MAPSINDOORS_VENUE_ID', // Replace with your actual venue ID
 });
 
-// Create a new element to host the floor selector.
+// Add MapsIndoors controls to the Google map (e.g., Floor Selector)
 const floorSelectorElement = document.createElement('div');
-
-// Create a new FloorSelector instance, linking it to the HTML element and the main MapsIndoors instance
 new mapsindoors.FloorSelector(floorSelectorElement, mapsIndoorsInstance);
-
-// Get the underlying Google Maps map instance from the MapsIndoors map view
-const googleMapsInstance = mapViewInstance.getMap();
-
-// Add the floor selector to the Google Maps controls
-googleMapsInstance.controls[google.maps.ControlPosition.RIGHT_TOP].push(floorSelectorElement);
+const googleMapInstance = mapViewInstance.getMap();
+googleMapInstance.controls[google.maps.ControlPosition.TOP_RIGHT].push(floorSelectorElement);
